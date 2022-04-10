fc1341519608fd7929cea6681d97c731d99cc42c
jake <palatable7@naver.com>
2022-04-10 23:53:49 +0900

    Implement

--- ExAVPlayerLayer/ViewController.swift
+++ ExAVPlayerLayer/ViewController.swift
@@ -6,14 +6,90 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+  private lazy var videoView: VideoView = {
+    let view = VideoView(url: "https://bitmovin-a.akamaihd.net/content/art-of-motion_drm/m3u8s/11331.m3u8")
+    view.translatesAutoresizingMaskIntoConstraints = false
+    self.view.addSubview(view)
+    return view
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    NSLayoutConstraint.activate([
+      self.videoView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 70),
+      self.videoView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -70),
+      self.videoView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -200),
+      self.videoView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 200),
+    ])
   }
-
-
 }
 
+final class VideoView: UIView {
+  private lazy var videoBackgroundView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .systemGray
+    view.translatesAutoresizingMaskIntoConstraints = false
+    self.addSubview(view)
+    return view
+  }()
+  private lazy var slider: UISlider = {
+    let slider = UISlider()
+    slider.translatesAutoresizingMaskIntoConstraints = false
+    self.addSubview(slider)
+    return slider
+  }()
+  
+  private var player: AVPlayer?
+  private let url: String
+  
+  init(url: String) {
+    self.url = url
+    super.init(frame: .zero)
+    guard let url = URL(string: url) else { return }
+    
+    NSLayoutConstraint.activate([
+      self.videoBackgroundView.leftAnchor.constraint(equalTo: self.leftAnchor),
+      self.videoBackgroundView.rightAnchor.constraint(equalTo: self.rightAnchor),
+      self.videoBackgroundView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50),
+      self.videoBackgroundView.topAnchor.constraint(equalTo: self.topAnchor),
+    ])
+    
+    NSLayoutConstraint.activate([
+      self.slider.topAnchor.constraint(equalTo: self.videoBackgroundView.bottomAnchor, constant: 16),
+      self.slider.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 16),
+      self.slider.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16),
+    ])
+  
+    let asset = AVAsset(url: url)
+    self.slider.minimumValue = 0
+    self.slider.maximumValue = Float(CMTimeGetSeconds(asset.duration))
+    self.slider.addTarget(self, action: #selector(changeValue), for: .valueChanged)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+
+    guard let url = URL(string: self.url) else { return }
+    self.player = AVPlayer(url: url)
+    let playerLayer = AVPlayerLayer(player: self.player)
+    playerLayer.frame = self.videoBackgroundView.bounds
+    playerLayer.videoGravity = .resizeAspectFill
+    self.videoBackgroundView.layer.addSublayer(playerLayer)
+    self.player?.play()
+  }
+  
+  
+  @objc private func changeValue() {
+    self.player?.seek(to: CMTime(seconds: Double(self.slider.value), preferredTimescale: Int32(NSEC_PER_SEC)), completionHandler: { _ in
+    })
+  }
+}
